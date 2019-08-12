57a1492483248ae5d1526a6d0bf0a33410d2e9b2
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-08-11 23:15:05 -0400

    Updates

---
+++ precache-manifest.d6a4a071a26d4f0b3d45bf28e56418e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e6b8411629a9cfb83a31a734037ba4",
+    "url": "/guessing-game/index.html"
+  },
+  {
+    "revision": "5325fd11dd9ff3beea94",
+    "url": "/guessing-game/static/css/main.fce9bc68.chunk.css"
+  },
+  {
+    "revision": "752d2dc502ba5cfd2301",
+    "url": "/guessing-game/static/js/2.006d76b9.chunk.js"
+  },
+  {
+    "revision": "5325fd11dd9ff3beea94",
+    "url": "/guessing-game/static/js/main.a35b6bda.chunk.js"
+  },
+  {
+    "revision": "fe781634196fca528cbd",
+    "url": "/guessing-game/static/js/runtime~main.4e9801d6.js"
+  }
+]);
